0c19ee2bf3f942c78c5fac55d3f5eefaf0d4c492
Kasper Krunderup Jakobsen <kasperkrunderup@gmail.com>
2024-09-11 07:52:01 +0200

    laptop

--- ProtDiffusion/dataset/make_dirs.py
+++ ProtDiffusion/dataset/make_dirs.py
@@ -4,32 +4,34 @@ from tqdm import tqdm
 from datasets import load_dataset
 import gc
 
-def make_directories(output_dir):
-    dataset = load_dataset('/home/kkj/ProtDiffusion/datasets/SPARQL_UniRefALL', split='train', streaming=True).select_columns(['cluster50id','kingdom'])
+def make_directories(output_dir, chunk_size = 10000):
+    chunks = pd.read_csv('/home/kkj/ProtDiffusion/datasets/UniRef50_cluster_ids_and_kingdom.csv', chunksize=chunk_size)
     
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
     
-    for example in tqdm(dataset, total = 200915939, desc='Making directories'):
-        id = example['cluster50id']
-        kingdom = example['kingdom']
-        file_path = os.path.join(output_dir, f'{id}.csv')
-        new_row = {
-            'cluster50id': id,
-            'kingdom': kingdom,
-            'proteinid': [],
-            'sequence': [],
-            'length': []
-        }
-        pd.DataFrame([new_row]).to_csv(file_path, index=False)
-        
-        # Explicitly delete variables and trigger garbage collection
-        del example
-        del id
-        del kingdom
-        del file_path
-        del new_row
+    for chunk in tqdm(chunks, total = 50000000//chunk_size, desc='Making directories'): # Total len is approximate
+        for example in chunk.iterrows():
+            cluster_id = example[1]['cluster50id']
+            kingdom = example[1][' kingdomid']
+            if kingdom == 2: # Bacteria
+                kingdom = 0
+            elif kingdom == 2759: # Eukaryota
+                kingdom = 1
+            else:
+                raise ValueError(f'Unknown kingdom: {kingdom}')
+            file_path = os.path.join(output_dir, f'{cluster_id}.csv')
+            new_row = {
+                'cluster50id': cluster_id,
+                'kingdom': kingdom,
+                'proteinid': [],
+                'input_ids': [],
+                'length': []
+            }
+            pd.DataFrame([new_row]).to_csv(file_path, index=False)
+        del chunk
+        gc.collect()
 
 # Example usage:
-output_dir = '/home/kkj/ProtDiffusion/datasets/SPARQL_UniRefALL_grouped50'
+output_dir = '/home/kkj/ProtDiffusion/datasets/SPARQL_UniRefALL_grouped50_inital'
 make_directories(output_dir)

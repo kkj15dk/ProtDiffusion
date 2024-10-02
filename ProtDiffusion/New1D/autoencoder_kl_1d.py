fix gradient scaling

ProtDiffusion/New1D/autoencoder_kl_1d.py
@@ -195,29 +195,15 @@ class AutoencoderKL1D(ModelMixin, ConfigMixin, FromOriginalModelMixin):
         self,
         output: AutoencoderKLOutput1D, 
         input_ids: torch.Tensor,
-        sequence_length_reduction: str = 'mean' # Reduction along the L dimension. 'sum' has been implemented to be able to do gradient accumulation and afterwards scale the gradients. https://www.reddit.com/r/MachineLearning/comments/1acbzrx/d_gradient_accumulation_should_not_be_used_with/
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         
         ce_loss = nn.functional.cross_entropy(output.sample, input_ids, reduction='none') # B, L
         ce_loss = ce_loss * output.attention_masks[0] # B, L
-
-        if sequence_length_reduction == 'mean':
-            ce_loss = torch.sum(ce_loss) / output.attention_masks[0].sum()
-        elif sequence_length_reduction == 'sum':
-            ce_loss = torch.sum(ce_loss)
-        else:
-            raise NotImplementedError(f'Reduction "{sequence_length_reduction}" not implemented, choose between "sum" and "mean"')
-
+        ce_loss = torch.sum(ce_loss) / output.attention_masks[0].sum()
         
         kl_loss = output.latent_dist.kl()
-        kl_loss = kl_loss * output.attention_masks[-1]
-
-        if sequence_length_reduction == 'mean':
-            kl_loss = torch.sum(kl_loss) / output.attention_masks[-1].sum()
-        elif sequence_length_reduction == 'sum':
-            kl_loss = torch.sum(kl_loss)
-        else:
-            raise NotImplementedError(f'Reduction "{sequence_length_reduction}" not implemented, choose between "sum" and "mean"')
+        kl_loss = kl_loss * output.attention_masks[-1] # B, L
+        kl_loss = torch.sum(kl_loss) / output.attention_masks[-1].sum()
 
         return ce_loss, kl_loss
 

ProtDiffusion/training_utils.py
@@ -532,7 +532,12 @@ class VAETrainer:
                 probs = F.softmax(logoplot_sample, dim=0).cpu().numpy()
                 pool = mp.Pool(1)
                 pool.apply_async(make_logoplot, 
-                                args=(probs, logoplot_sample_id, f"{test_dir}/{name}_probs_{logoplot_sample_id}.png"),
+                                args=(
+                                    probs, 
+                                    logoplot_sample_id, 
+                                    f"{test_dir}/{name}_probs_{logoplot_sample_id}.png", 
+                                    self.tokenizer.decode(range(self.tokenizer.vocab_size)),
+                                ),
                                 error_callback=lambda e: print(e),
                                 callback=lambda _: pool.close(),
                 )
@@ -646,13 +651,14 @@ class VAETrainer:
                                     sample_posterior = True, # Should be set to true in training
                     )
                     
-                    ce_loss, kl_loss = self.model.loss_fn(output, input, sequence_length_reduction = 'sum') # https://www.reddit.com/r/MachineLearning/comments/1acbzrx/d_gradient_accumulation_should_not_be_used_with/
-                    kl_loss = kl_loss * self.config.pad_to_multiple_of # When using sum, scale the kl_loss appropriately. It will be divided by n_tokens when the gradients are scaled. There is a factor of pad_to_multiple_of fewer tokens in the latent dimension
+                    ce_loss, kl_loss = self.model.loss_fn(output, input) 
 
                     kl_weight = self.config.kl_weight * min(1.0, self.training_variables.global_step / (self.config.kl_warmup_steps * self.config.gradient_accumulation_steps))
 
                     loss = ce_loss + kl_loss * kl_weight
-                    self.accelerator.backward(loss)
+                    loss_back = loss * attention_mask.sum() # https://www.reddit.com/r/MachineLearning/comments/1acbzrx/d_gradient_accumulation_should_not_be_used_with/
+
+                    self.accelerator.backward(loss_back)
 
                     if self.accelerator.sync_gradients:
                         self.scale_gradients(self.model, n_tokens)

ProtDiffusion/visualization_utils.py
@@ -6,7 +6,6 @@ import numpy as np
 import pandas as pd
 import logomaker
 import matplotlib.pyplot as plt
-import time
 
 from logomaker.src.colors import get_color_dict
 
@@ -28,8 +27,7 @@ def make_color_dict(color_scheme:str = 'weblogo_protein', cs:str = "-[]ACDEFGHIK
 
 # %%
 @torch.no_grad()
-def make_logoplot(array, label:str, png_path:str, positions_per_line:int = 60, width:int = 100, ylim:tuple = (-0.1,1.1), dpi:int = 100, characters:str = "-[]ACDEFGHIKLMNPQRSTVWY"):
-    start_time = time.time()
+def make_logoplot(array, label:str, png_path:str, characters:str = "-[]ACDEFGHIKLMNPQRSTVWY", positions_per_line:int = 60, width:int = 100, ylim:tuple = (-0.1,1.1), dpi:int = 100):
     assert array.ndim == 2
 
     amino_acids = list(characters)
@@ -71,8 +69,6 @@ def make_logoplot(array, label:str, png_path:str, positions_per_line:int = 60, w
 
     gc.collect()  # Force garbage collection
 
-    end_time = time.time()
     print(f'Finished making logoplot for {label}')
-    print(f'Time taken: {end_time - start_time:.2f} seconds')
 
     return
4ea94a49005269b96409080347c1aecfc6adc528
Kasper Krunderup Jakobsen <kasperkrunderup@gmail.com>
2024-11-19 10:51:39 +0100

    HPC variables

--- train_VAE.py
+++ train_VAE.py
@@ -9,25 +9,25 @@ from ProtDiffusion.models.autoencoder_kl_1d import AutoencoderKL1D
 import os
 
 config = VAETrainingConfig(
-    num_epochs=10, # the number of epochs to train for
-    batch_size=128, # 24 batch size seems to be the max with 16384 as max_len for 32 GB GPU right now. With batch_size=32, it crashes wit CUDA OOM error, TODO: Should look into memory management optimisation.
+    num_epochs=12, # the number of epochs to train for
+    batch_size=64, # 24 batch size seems to be the max with 16384 as max_len for 32 GB GPU right now. With batch_size=32, it crashes wit CUDA OOM error, TODO: Should look into memory management optimisation.
     mega_batch=240,
     pad_to_multiple_of=8,
-    gradient_accumulation_steps=2,
+    gradient_accumulation_steps=32,
     optimizer = "AdamW",
     learning_rate=4e-6,
-    lr_warmup_steps=5000,
+    lr_warmup_steps=10000,
     lr_schedule='cosine_10x_decay',
-    kl_warmup_steps=10000,
-    kl_weight=1e-7, # https://www.reddit.com/r/StableDiffusion/comments/1bo8d3k/why_not_use_ae_rather_than_vae_in_the_stable/
-    kl_schedule='constant_with_restarts',
-    save_image_model_steps=50000,
-    output_dir=os.path.join("output","protein-VAE-UniRef50_v24.1_latent-2"), # the model name locally and on the HF Hub
+    kl_warmup_steps=20000,
+    kl_weight=1e-6, # https://www.reddit.com/r/StableDiffusion/comments/1bo8d3k/why_not_use_ae_rather_than_vae_in_the_stable/
+    kl_schedule='constant_with_warmup',
+    save_image_model_steps=100000,
+    output_dir=os.path.join("output","protein-VAE-UniRef50_v24.4_latent-4_conv_transpose"), # the model name locally and on the HF Hub
     total_checkpoints_limit=1, # the maximum number of checkpoints to keep
     gradient_clip_val=1.0, # 5.0,
-    max_len=1024, # 512 * 8 ((2**3))
+    max_len=2048, # 512 * 8 ((2**3))
     max_len_start=256,
-    max_len_doubling_steps=2000000,
+    max_len_doubling_steps=1500000,
     ema_decay=0.9999,
     ema_update_after=10000,
     ema_update_every=100,
@@ -42,6 +42,7 @@ dataset = load_from_disk('/work3/s204514/UniRef50_grouped')
 dataset = dataset.shuffle(config.seed)
 
 train_dataset = dataset['train']
+train_dataset = train_dataset.shuffle(config.seed + 1) # shuffe again for restart
 val_dataset = dataset['valid']
 test_dataset = dataset['test']
 
@@ -68,6 +69,7 @@ train_dataloader = make_clustered_dataloader(config.batch_size,
                                              max_len=config.max_len_start,
                                              num_workers=num_workers,
                                              pad_to_multiple_of=config.pad_to_multiple_of,
+                                             random_padding=True,
 )
 val_dataloader = make_clustered_dataloader(config.batch_size,
                                            config.mega_batch,
@@ -76,6 +78,7 @@ val_dataloader = make_clustered_dataloader(config.batch_size,
                                            max_len=config.max_len, 
                                            num_workers=1,
                                            pad_to_multiple_of=config.pad_to_multiple_of,
+                                           random_padding=True,
 )
 test_dataloader = make_clustered_dataloader(config.batch_size,
                                             config.mega_batch,
@@ -84,6 +87,7 @@ test_dataloader = make_clustered_dataloader(config.batch_size,
                                             max_len=config.max_len, 
                                             num_workers=1,
                                             pad_to_multiple_of=config.pad_to_multiple_of,
+                                            random_padding=True,
 )
 print("length of train dataloader: ", len(train_dataloader))
 print("length of val dataloader: ", len(val_dataloader))
@@ -112,7 +116,7 @@ model = AutoencoderKL1D(
     layers_per_block=2,  # how many ResNet layers to use per UNet block
     transformer_layers_per_block=1, # how many transformer layers to use per ResNet layer. Not implemented yet.
 
-    latent_channels=2,  # the dimensionality of the latent space
+    latent_channels=4,  # the dimensionality of the latent space
 
     num_attention_heads=1,  # the number of attention heads in the spatial self-attention blocks
     upsample_type="conv_transpose", # the type of upsampling to use, either 'conv' (and nearest neighbor) or 'conv_transpose'
@@ -132,4 +136,4 @@ Trainer = VAETrainer(model,
 
 # %%
 if __name__ == '__main__':
-    Trainer.train()
+    Trainer.train(from_checkpoint='/zhome/fb/0/155603/output/protein-VAE-UniRef50_v24.2_latent-4_conv_transpose/checkpoints/checkpoint_17')
